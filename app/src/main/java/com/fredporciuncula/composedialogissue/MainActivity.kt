37405cd7cce04ece25677e2c1915c60932004c81
Fred Porciúncula <tfcporciuncula@gmail.com>
2022-04-22 17:32:52 +0200

    Add dialog code

--- app/src/main/java/com/fredporciuncula/composedialogissue/MainActivity.kt
+++ app/src/main/java/com/fredporciuncula/composedialogissue/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.fredporciuncula.composedialogissue.ui.theme.ComposeDialogIssueTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,12 +28,36 @@ class MainActivity : ComponentActivity() {
         }
       }
     }
+
+    // comment the entire setContent() call above and uncomment the line below to see a regular dialog
+//    android.app.AlertDialog.Builder(this).setMessage("Old dialog (non compose) sw320dp").create().show()
   }
 }
 
 @Composable
 fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+  Dialog(onDismissRequest = {}) {
+    Surface(shape = MaterialTheme.shapes.medium) {
+      Text(
+        text = "Compose dialog ",
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(vertical = 100.dp),
+      )
+    }
+  }
+
+  // or:
+//  AlertDialog(
+//    onDismissRequest = {},
+//    buttons = {},
+//    text = {
+//      Text(
+//        text = "Compose alert dialog ",
+//        modifier = Modifier.fillMaxWidth(),
+//      )
+//    },
+//  )
 }
 
 @Preview(showBackground = true)
